9f20a255b99404e03b3cc5a8c6f1f363bba7a46f
NORRAN MARTINS <norranmartins8@gmail.com>
2026-05-20 14:25:17 -0300

    Refactor SQL query to include partners and counts

--- banco/script/consultas.sql
+++ banco/script/consultas.sql
@@ -1,13 +1,12 @@
-
--- Pesquisa os dados de uma Empresa pré definidos --
-SELECT e.cnpj, e.razao_social
+--Busca empresa pelo CNPJ e lista seus sócios:
+SELECT 
+    e.cnpj,
+    e.razao_social,
+    COUNT(p.cpf) OVER (PARTITION BY e.cnpj) AS quantidade_socios,
+    s.nome,
+    s.cpf
 FROM empresa e
-WHERE e.cnpj = '64342828000100';
--------------------------------------------
---   historico De Cnpj ja pesquisados --
-SELECT h.cnpj_pesquisado, e.razao_social
-FROM historico_pesquisas h
-JOIN empresa e ON e.cnpj = h.cnpj_pesquisado
-WHERE h.usuario_id = 1
-ORDER BY h.data_pesquisa DESC
-LIMIT 4;
+LEFT JOIN participacoes p ON p.empresa_cnpj = e.cnpj
+LEFT JOIN socio s ON s.cpf = p.cpf
+WHERE e.cnpj = '64342828000100'
+ORDER BY s.nome;
